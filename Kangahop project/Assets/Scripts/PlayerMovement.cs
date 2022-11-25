df45e1287b0488c43cce86e4b5b64f299011213b
Christoo2003 <113982896+Christoo2003@users.noreply.github.com>
2022-11-25 23:40:02 +0000

    Project

---
+++ Kangahop project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+     public float speed = 20.0f;
+     private float horizontalInput;
+     private float forwardInput;
+     private Rigidbody playerRb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+   horizontalInput = Input.GetAxis("Horizontal");
+   forwardInput = Input.GetAxis("Vertical");
+
+   transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+   transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+     
+     if(Input.GetKeyDown(KeyCode.Space)) {
+     playerRb.AddForce(Vector3.up * 50, ForceMode.Impulse);
+
+     }
+
+
+
+ 
+    }
+}
